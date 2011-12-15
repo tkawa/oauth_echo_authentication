2ab84966576b8db97b9a77283545c3aab8afc084
tkawa <tkawa@4bit.net>
2011-12-15 21:59:54 +0900

    add a class method and some fixes

--- lib/oauth_echo_authentication.rb
+++ lib/oauth_echo_authentication.rb
@@ -2,27 +2,63 @@ require 'active_support/concern'
 require 'open-uri'
 
 module OauthEchoAuthentication
-  extend ActiveSupport::Concern
+  extend self
 
-  module ClassMethods
-    # for instant
-  end
-  def authenticate_with_oauth_echo
-    @provider = request.headers['X-Auth-Service-Provider'] || 'https://api.twitter.com/1/account/verify_credentials.json'
-    credentials = request.headers['X-Verify-Credentials-Authorization']
-    unless credentials && credentials =~ /\AOAuth\s+/
-      head 400 # Bad Request
-      return
+  WELLKNOWN_PROVIDERS = { :twitter => 'https://api.twitter.com/1/account/verify_credentials.json' }
+
+  module ControllerMethods
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def oauth_echo_authenticate_with(specified_provider, options = {})
+        before_filter(options) do
+          oauth_echo_authenticate_with(specified_provider)
+        end
+      end
     end
-    res = OpenURI.open_uri(@provider, 'Authorization' => credentials)
-    json = res.read
+
+    def oauth_echo_authenticate_with(specified_provider)
+      provider = request.env['X-Auth-Service-Provider']
+      raise AuthError.new('Unauthorized', 401) unless provider
+      unless provider == WELLKNOWN_PROVIDERS[specified_provider]
+        raise AuthError.new('Forbidden: unsupported provider', 403)
+      end
+      @current_user = authenticate_with_oauth_echo!
+    rescue OpenURI::HTTPError => e
+      render json: JSON.parse(e.io.read), status: e.io.status[0].to_i
+    rescue AuthError => e
+      head e.status
+    end
+
+    def authenticate_with_oauth_echo!
+      provider = request.headers['X-Auth-Service-Provider']
+      credentials = request.headers['X-Verify-Credentials-Authorization']
+      unless provider && credentials && credentials =~ /\AOAuth\s+/
+        raise AuthError.new('Unauthorized: invalid auth headers', 401)
+      end
+      OauthEchoAuthentication.authenticate(provider, credentials)
+    end
+
+    def authenticate_with_oauth_echo
+      authenticate_with_oauth_echo! rescue nil
+    end
+  end
+
+  def authenticate(provider, credentials)
+    res = OpenURI.open_uri(provider, 'Authorization' => credentials)
+    json = JSON.parse(res.read)
     res.close
     raise OpenURI::HTTPError.new(res.status.join(' '), res) unless res.status[0] == '200'
-    @user = JSON.parse(json)
-  rescue OpenURI::HTTPError => e
-    render json: JSON.parse(e.io.read), status: e.io.status[0].to_i
-    return
+    json
+  end
+
+  class AuthError < StandardError
+    def initialize(message, status)
+      super(message)
+      @status = status
+    end
+    attr_reader :status
   end
 end
 
-ActionController::Base.send :include, OauthEchoAuthentication
+ActionController::Base.send :include, OauthEchoAuthentication::ControllerMethods
